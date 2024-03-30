a3bcd76b98b8427e9d35fa87fc5c37ce9daf58f9
sauchm <93037524+cschenhm@users.noreply.github.com>
2024-03-30 20:59:14 +0800

    Update statistic.m

--- evaluations/Evaluation_DID-Data_DDN-Data/statistic.m
+++ evaluations/Evaluation_DID-Data_DDN-Data/statistic.m
@@ -1,19 +1,21 @@
 clear all;
 ts =0;
 tp =0;
-for i=1:1200                         % the number of testing samples DID-Data
-%for i=1:1400                          % the number of testing samples DDN-Data
-   x_true=im2double(imread(strcat('.\gt\',sprintf('%d.png',i))));  % groundtruth 
+% for i=1:1200                         % the number of testing samples DID-Data
+for i=1:1400                          % the number of testing samples DDN-Data
+  x_true=im2double(imread(strcat('./gt/DDN-Data/target/',sprintf('%d.jpg',i))));  % groundtruth 
+   % x_true=im2double(imread(strcat('./gt/DID-Data/target/',sprintf('%d.jpg',i))));  % groundtruth 
    x_true = rgb2ycbcr(x_true);
    x_true = x_true(:,:,1); 
-   x = im2double(imread(strcat('.\results\',sprintf('%d.png',i))));     %reconstructed image
+   x = im2double(imread(strcat('./results/DDN-Data/',sprintf('%d.png',i))));     %reconstructed image
+ %  x = im2double(imread(strcat('./results/DID-Data/',sprintf('%d.png',i))));     %reconstructed image
    x = rgb2ycbcr(x);
    x = x(:,:,1);
    tp= tp+ psnr(x,x_true);
    ts= ts+ssim(x*255,x_true*255);
 end
-fprintf('psnr=%6.4f, ssim=%6.4f\n',tp/1200,ts/1200)                          % the number of testing samples DID-Data
-%fprintf('psnr=%6.4f, ssim=%6.4f\n',tp/1400,ts/1400)                          % the number of testing samples DDN-Data
+% fprintf('psnr=%6.4f, ssim=%6.4f\n',tp/1200,ts/1200)                          % the number of testing samples DID-Data
+fprintf('psnr=%6.4f, ssim=%6.4f\n',tp/1400,ts/1400)                          % the number of testing samples DDN-Data
 
 
 
